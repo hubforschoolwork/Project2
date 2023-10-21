5ac34e4243d77a39c00b23a261f0847939722bc4
hubforschoolwork <feliciaj6@sbcglobal.net>
2023-10-21 16:50:04 -0700

    Added code to js page

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,45 @@
+
+//Click Listener
+
+document.addEventListener("click", function (event) {
+    // Checking if the button was clicked
+    if (!event.target.matches("#button")) return;
   
+    console.log("Button was clicked!");
+  });
+
+
+//Fetch
+
+fetch("<https://official-joke-api.appspot.com/random_joke>")
+.then((response) => response.json())
+.then((data) => renderJoke(data));
+.catch(() => renderError());
+});
+
+
+//Taking object received from Endpoint and add elements InnerHTML
+
+function renderJoke(data) {
+    //Getting elements
+    
+    const setup = document.getElementById("setup");
+    const punchline = document.getElementById("punchline");
+
+    const error = document.getElementById("error");
+    error.innerHTML = "";
+
+    setup.innerHTML = data.setup;
+    punchline.innerHTML = data.punchline;
+  }
+
+
+
+  function renderError() {
+    const error = document.getElementById("error");
+    error.innerHTML = "Whoops, something went wrong. Please try again later!";
+  }
+
+
 
   
