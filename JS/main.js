944cc967fbca111be5fe806f6477baf46ac1099e
hubforschoolwork <feliciaj6@sbcglobal.net>
2023-10-22 11:51:08 -0700

    Added Giphy API key

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,13 @@
+//Giphy Search Endpoint Code
+//javascript, jQuery
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
+
+
+
+
+
 
 //Click Listener
 
