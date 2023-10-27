d14f0f48c7be95f894f71fd420ca16e633a3aa35
hubforschoolwork <feliciaj6@sbcglobal.net>
2023-10-27 14:24:29 -0700

    Adjust css styling

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", init);
         document.getElementById("btnSearch").addEventListener("click", ev => {
           ev.preventDefault(); 
 
-          let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=9&q=`;
+          let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=6&q=`;
           let str = document.getElementById("search").value.trim();
           url = url.concat(str);
           console.log(url);
